ac16b1ac934f21286972aae08345409d32b4272a
David Moodie <davidmoodie12@gmail.com>
2018-05-06 02:03:29 +0100

    Make the demo better

--- example/demo.js
+++ example/demo.js
@@ -12,7 +12,7 @@ upload.addEventListener(
     const files = [...evt.target.files]
     compress
       .compress(files, {
-        size: 0.250, // the max size in MB, defaults to 2MB
+        size: 0.25, // the max size in MB, defaults to 2MB
         quality: 0.75, // the quality of the image, max is 1,
         maxWidth: 1920, // the max width of the output image, defaults to 1920px
         maxHeight: 1920, // the max height of the output image, defaults to 1920px
@@ -31,10 +31,27 @@ upload.addEventListener(
           iterations,
           sizeReducedInPercent,
           elapsedTimeInSeconds,
-          alt
+          alt,
+          quality
         } = img
 
-        output.innerHTML = `<b>Start Size:</b> ${initialSizeInMb} MB <br/><b>End Size:</b> ${endSizeInMb} MB <br/><b>Compression Cycles:</b> ${iterations} <br/><b>Size Reduced:</b> ${sizeReducedInPercent} % <br/><b>File Name:</b> ${alt}`
+        output.innerHTML = `<strong>Start Size:</strong> ${initialSizeInMb} MB <br/>
+        <strong>End Size:</strong> ${endSizeInMb} MB <br/>
+        <strong>Compression Cycles:</strong> ${iterations} <br/>
+        <strong>Size Reduced:</strong> ${sizeReducedInPercent} % <br/>
+        <strong>File Name:</strong> ${alt} <br/>
+        <strong>Final quality:</strong> ${quality}`
+
+        output.innerHTML = `
+          <table>
+            <tr><th>Start Size:</th><td>${initialSizeInMb} MB</td></tr>
+            <tr><th>End Size:</th><td>${endSizeInMb} MB</td></tr>
+            <tr><th>Compression Cycles:</th><td>${iterations}</td></tr>
+            <tr><th>Size Reduced:</th><td>${sizeReducedInPercent} %</td></tr>
+            <tr><th>File Name:</th><td>${alt}</td></tr>
+            <tr><th>Final quality:</th><td>${quality}</td></tr>
+          </table>
+        `
       })
   },
   false
