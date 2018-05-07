a3e5233291eed7656f512ce50f43a7833649c80b
David Moodie <davidmoodie12@gmail.com>
2018-05-07 13:48:03 +0100

    Update example for v2

--- index.js
+++ index.js
@@ -35,9 +35,16 @@ upload.addEventListener(
     compress.compress(files).then((conversions) => {
       const { photo, info } = conversions[0]
 
-      preview.src = photo.data
-
       console.log({photo, info})
+
+      // Create an object URL which points to the photo Blob data
+      const objectUrl = URL.createObjectURL(photo.data)
+
+      // Set the preview img src to the object URL and wait for it to load
+      Compress.loadImageElement(preview, objectUrl).then(() => {
+        // Revoke the object URL to free up memory
+        URL.revokeObjectURL(objectUrl)
+      })
 
       displayObject(photo, 'output-photo')
       displayObject(info, 'output-info')
